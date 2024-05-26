ce9d7593d9010a5b402bc969dc4323f5cc7dcaf5
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-25 19:10:09 -0500

    completed player refactoring lesson

--- player/player.gd
+++ player/player.gd
@@ -16,12 +16,10 @@ var jump_count = 0
 var camera_instance
 
 func _ready():
-	camera_instance = player_camera.instantiate()
-	camera_instance.global_position.y = camera_height
-	get_tree().current_scene.add_child.call_deferred(camera_instance)
+	set_up_camera()
 	
 func _process(_delta: float) -> void:
-	camera_instance.global_position.x = global_position.x
+	update_camera_pos()
 
 func _physics_process(_delta: float) -> void:
 	var horizontal_input = (
@@ -32,6 +30,32 @@ func _physics_process(_delta: float) -> void:
 	velocity.x = horizontal_input * movement_speed
 	velocity.y += gravity
 	
+	handle_movement_state()
+	
+	move_and_slide()
+	
+	face_movement_direction(horizontal_input)
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	player_sprite.play("jump")
+
+func set_up_camera():
+	camera_instance = player_camera.instantiate()
+	camera_instance.global_position.y = camera_height
+	get_tree().current_scene.add_child.call_deferred(camera_instance)
+
+func update_camera_pos():
+	camera_instance.global_position.x = global_position.x
+
+func face_movement_direction(horizontal_input):
+	if not is_zero_approx(horizontal_input):
+		if horizontal_input < 0:
+			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
+		else:
+			player_sprite.scale = initial_sprite_scale
+
+func handle_movement_state():
 	var is_falling = velocity.y > 0.0 and not is_on_floor()
 	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
 	var is_double_jumping = Input.is_action_just_pressed("jump") and is_falling
@@ -50,9 +74,8 @@ func _physics_process(_delta: float) -> void:
 		velocity.y = 0.0
 	elif is_on_floor():
 		jump_count = 0
-	
-	move_and_slide()
-	
+		
+			
 	if is_jumping:
 		player_sprite.play("jump_start")
 	elif is_double_jumping:
@@ -63,14 +86,3 @@ func _physics_process(_delta: float) -> void:
 		player_sprite.play("fall")
 	elif is_idle:
 		player_sprite.play("idle")
-	
-	
-	if not is_zero_approx(horizontal_input):
-		if horizontal_input < 0:
-			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
-		else:
-			player_sprite.scale = initial_sprite_scale
-
-
-func _on_animated_sprite_2d_animation_finished() -> void:
-	player_sprite.play("jump")
