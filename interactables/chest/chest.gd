009e18ec619dac74dd6d63bf7b317dbacd1c42c9
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-27 16:24:14 -0500

    commpleted treasure chest lesson

---
+++ interactables/chest/chest.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var chest_locked: Sprite2D
+@export var chest_unlocked: Sprite2D
+@export var is_locked = true
+
+func _on_test_interact(state):
+	if state:
+		_on_interactable_interacted()
+
+func set_chest_properties():
+	chest_locked.visible = is_locked
+	chest_unlocked.visible = !is_locked
+
+func _on_interactable_interacted():
+	if is_locked:
+		is_locked = false
+	set_chest_properties()
+
+
+func _on_multiplayer_synchronizer_delta_synchronized() -> void:
+	set_chest_properties()
