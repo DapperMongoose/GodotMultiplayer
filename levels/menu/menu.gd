2ac7652d159a452a6b4f61f5e1b358bca538cac5
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-05-26 20:34:52 -0500

    completed starting the game lesson

--- levels/menu/menu.gd
+++ levels/menu/menu.gd
@@ -1,7 +1,12 @@
-extends Control
+extends Node
 
 @export var ip_line_edit: LineEdit
 @export var status_label: Label
+@export var level_container: Node
+@export var level_scene: PackedScene
+@export var not_connected_hbox: HBoxContainer
+@export var host_hbox: HBoxContainer
+@export var ui: Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,19 +15,35 @@ func _ready() -> void:
 
 
 func _on_host_button_pressed() -> void:
+	not_connected_hbox.hide()
+	host_hbox.show()
+	status_label.text = "Hosting!"
 	Lobby.create_game()
 
-
 func _on_join_button_pressed() -> void:
+	not_connected_hbox.hide()
 	Lobby.join_game(ip_line_edit.text)
 	status_label.text = "Connecting..."
 
-
 func _on_start_button_pressed() -> void:
-	pass # Replace with function body.
+	hide_menu.rpc()
+	change_level.call_deferred(level_scene)
+	
+	
+func change_level(scene):
+	for c in level_container.get_children():
+		level_container.remove_child(c)
+		c.queue_free()
+	level_container.add_child(scene.instantiate())
+
 
 func _on_connection_failed():
+	not_connected_hbox.show()
 	status_label.text = "Failed to connect"
 	
 func _on_connected_to_server():
 	status_label.text = "Connected!"
+	
+@rpc("call_local", "authority", "reliable")
+func hide_menu():
+	ui.hide()
